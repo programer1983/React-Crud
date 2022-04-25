e93123a92e60039c767b722611926f20ccf91067
programer1983 <programer972@gmail.com>
2022-04-25 12:29:02 +0300

    commit-3;

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-
+import {useState, useEffect} from "react"
 import './App.css';
 
 function App() {
+  const [user, setUser] = useState([])
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  const fetchData = async () => {
+    await fetch("https://jsonplaceholder.typicode.com/users")
+    .then((res) => res.json())
+    .then((data) => setUser(data))
+    .catch((err) => {
+      console.log(err)
+    })
+  }
+
+  console.log(user)
+
+
   return (
     <div className="App">
-      hello
+      <h3>React Crud Using Jsonplaceholder</h3>
     </div>
   );
 }
